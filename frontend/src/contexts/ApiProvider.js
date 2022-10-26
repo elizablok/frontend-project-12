546cde5fa10412642e2b2b2328b4d0fb4f468ea5
eliza <elizabet002@mail.ru>
2022-10-26 18:03:43 +0400

    changed api

--- frontend/src/contexts/ApiProvider.js
+++ frontend/src/contexts/ApiProvider.js
@@ -1,8 +1,6 @@
 import React, {
   useContext, createContext, useMemo,
 } from 'react';
-import { channelsActions } from '../slices/channelsSlice';
-import store from '../slices/index';
 
 const ApiContext = createContext({});
 const useApi = () => useContext(ApiContext);
@@ -16,12 +14,7 @@ const ApiProvider = ({ socket, children }) => {
       socket.emit('newChannel', payload);
     },
     removeChannel: (payload) => {
-      socket.emit('removeChannel', payload, (response) => {
-        const DEFAULT_CHANNEL_ID = 1;
-        store.dispatch(channelsActions.setCurrent(DEFAULT_CHANNEL_ID));
-
-        return response.data;
-      });
+      socket.emit('removeChannel', payload);
     },
     renameChannel: (payload) => {
       socket.emit('renameChannel', payload);
